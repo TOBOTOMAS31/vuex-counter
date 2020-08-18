899c962825d38b6470b0ba1a461c189358721204
TOBOTOMAS31 <thomas.couderc@live.fr>
2020-08-18 15:47:46 +0200

    create methods increment and decrement counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>{{ currentValue }}</h2>
+    <div>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      increment() {
+        this.$store.commit('INCREMENT_COUNTER');
+      },
+      decrement() {
+        this.$store.commit('DECREMENT_COUNTER');
+      }
+    },
+    computed: {
+      currentValue() {
+        return this.$store.state.currentValue;
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
